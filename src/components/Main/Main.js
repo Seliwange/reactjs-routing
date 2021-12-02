f8a509ef4f25579ee982b8a5f5f1934ded8990f2
Seliwange <yul.yaremchuk@gmail.com>
2021-12-02 13:33:40 +0200

    feat: delete property

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,12 +3,11 @@ import Home from '../Home/Home';
 import Contacts from '../Contacts/Contacts';
 import Posts from '../Posts/Posts';
 import Photos from '../Photos/Photos';
-import data from '../Contacts/Data';
 
 export default function Main(){
     return (
         <Routes>
-            <Route path="contacts" element={<Contacts contacts={data} />} />
+            <Route path="contacts" element={<Contacts />} />
             <Route path="posts" element={<Posts />} />
             <Route path="photos" element={<Photos />} />
             <Route path="/" exact element={<Home />} />
